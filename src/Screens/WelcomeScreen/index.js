976eb9369d95a01cb536b28595a03ce6f7e5cde7
ShinHyunJong <pleiades@algorithmlabs.co.kr>
2019-04-08 15:20:10 +0900

    Welcome Screen 폰트 수정

--- src/Screens/WelcomeScreen/index.js
+++ src/Screens/WelcomeScreen/index.js
@@ -46,8 +46,10 @@ export class WelcomeScreen extends Component {
       <View style={styles.container}>
         <View style={styles.header}>
           <Image style={styles.logo} source={WaveLogoWhite} />
-          <Text style={styles.header__title}>웨이브</Text>
-          <Text style={styles.header__subtitle}>나만을 위한 편집샵</Text>
+          <Text style={styles.header__title}>스타일 클라우드</Text>
+          <Text style={styles.header__subtitle}>
+            이제는 나만의 옷장을 구독하세요.
+          </Text>
         </View>
         <View style={styles.footer}>
           <FullWidthButton icon="facebook-f" content="페이스북으로 로그인" />
